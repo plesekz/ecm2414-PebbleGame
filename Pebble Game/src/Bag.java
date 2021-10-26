841b12323d282b176e7212b78d69319ec0daed60
Zdeněk Plešek <7denek.p1esek@gmail.com>
2021-10-26 17:54:59 +0100

    naming bags

--- Pebble Game/src/Bag.java
+++ Pebble Game/src/Bag.java
@@ -3,8 +3,12 @@ import java.util.ArrayList;
 public class Bag {
 	ArrayList<Integer> content = new ArrayList<Integer>();
 	Bag pairedBag;
+	String name;
 	
 	public void putInPebble(Integer i) {
 		content.add(i);
+	}
+	public String getName() {
+		return name;
 	}
 }
