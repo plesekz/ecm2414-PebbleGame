1005fafb0b5a47932dac8a6dbf536aa70cdf3f35
Riordan Sabatini <rs845@exeter.ac.uk>
2021-10-26 17:27:54 +0100

    PebbleGame Implementation Start

---
+++ Pebble Game/src/PebbleGame.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class PebbleGame {
+
+
+    private class Player{
+        ArrayList<Integer> hand;
+        ArrayList<BlackBag> avaiableBags;
+
+
+        public Player(ArrayList<BlackBag> bagsFromFile){
+            avaiableBags = bagsFromFile;
+        }
+        public void run(){
+
+            Boolean done = false;
+            while(!done){
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
